0be7921e262088a9be8fef9baa2ca1762c2115de
Daniel Calvin <arockne2003@yahoo.com>
2021-05-21 00:23:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const breakOut = (array, changeValue, stopValue) => {
   }
   return array;
 };
+
+const keepGoing = (array, changeValue, skipValue) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
+  }
+};
